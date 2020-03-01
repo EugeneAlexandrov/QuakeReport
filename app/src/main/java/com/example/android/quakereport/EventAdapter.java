5e0d5a7cebe1d1ff6155a107b3d5bace7b372025
Eugen <Alexandrov>
2020-03-01 22:37:32 +0300

    форматирование строк

--- app/src/main/java/com/example/android/quakereport/EventAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EventAdapter.java
@@ -10,11 +10,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 class EventAdapter extends ArrayAdapter<Earthquake> {
     Context mcontext;
     int prev_position = 0;
+    String place;
 
     public EventAdapter(@NonNull Context context, @NonNull List objects) {
         super(context, 0, objects);
@@ -29,12 +35,50 @@ class EventAdapter extends ArrayAdapter<Earthquake> {
             listview = LayoutInflater.from(mcontext).inflate(R.layout.item, parent, false);
         }
         Earthquake curentEarthquake = getItem(position);
+        Double mag = curentEarthquake.getmMag();
+        place = curentEarthquake.getmCity();
+        Long date = curentEarthquake.getmDate();
+
         TextView mag_text = (TextView) listview.findViewById(R.id.mag_text);
-        mag_text.setText(Double.toString(curentEarthquake.getmMag()));
+        mag_text.setText(Double.toString(mag));
+
         TextView city_text = (TextView) listview.findViewById(R.id.city_text);
-        city_text.setText(curentEarthquake.getmCity());
+
+        TextView distance_text = (TextView) listview.findViewById(R.id.distance_text);
+        String distance;
+        String city;
+        if (place.contains(" of")){
+           String[] result=place.split(" of");
+            distance=result[0]+" of";
+            city=result[1];
+
+        } else {
+            city=place;
+            distance="near the";
+        }
+        city_text.setText(city);
+        distance_text.setText(distance);
+
+        Date dateObject = new Date(curentEarthquake.getmDate());
+        String formatedDate = formatDate(dateObject);
         TextView date_text = (TextView) listview.findViewById(R.id.date_text);
-        date_text.setText(Long.toString(curentEarthquake.getmDate()));
+        date_text.setText(formatedDate);
+        String formatedtime = formatTime(dateObject);
+        TextView time_text = (TextView) listview.findViewById(R.id.time_text);
+        time_text.setText(formatedtime);
         return listview;
+    }
+
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+    }
+
+    /**
+     * Return the formatted date string (i.e. "4:30 PM") from a Date object.
+     */
+    private String formatTime(Date dateObject) {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+        return timeFormat.format(dateObject);
     }
 }
